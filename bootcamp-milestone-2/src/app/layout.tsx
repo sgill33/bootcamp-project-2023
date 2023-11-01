d2c8a7f698df5a49d3c9b6eec9b2ae90767de1c6
sgill33 <sgill33@calpoly.edu>
2023-11-01 14:43:21 -0700

    added navbar to layout

--- bootcamp-milestone-2/src/app/layout.tsx
+++ bootcamp-milestone-2/src/app/layout.tsx
@@ -1,13 +1,15 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google"; // You can change the font to anything you want.
+import Navbar from "./components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "[name] Personal Website",
-  description: "A personal website for [name].",
+  title: "Shawn's Site",
+  description: "Shawn's Personal Site",
 };
+
 
 export default function RootLayout({
   children,
@@ -16,7 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar/>
+        {children}
+        </body>
     </html>
   );
 }
