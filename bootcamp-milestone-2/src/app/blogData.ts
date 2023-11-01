488056f64fe5983b2e33b46322192c7d0ae82e0a
sgill33 <sgill33@calpoly.edu>
2023-11-01 14:42:24 -0700

    added blog structure to blogData.ts

---
+++ bootcamp-milestone-2/src/app/blogData.ts
@@ -0,0 +1,21 @@
+export interface Blog {
+
+}
+
+const blogs: Blog[] = [
+    {
+        title: "My Hobbies",
+        date: "10-20-2023",
+        description: "Get to know about my hobbies",
+        slug: "blog2.html"
+    },
+    {
+        title: "My First Blog",
+        date: "10-19-2023",
+        description: "This blog is my very first",
+        slug: "blog1.html"
+    }
+
+];
+
+export default blogs;
