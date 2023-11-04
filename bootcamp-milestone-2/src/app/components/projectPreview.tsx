753e65d3f8ef1a366285b03d6e239d7d9a3ef42b
sgill33 <sgill33@calpoly.edu>
2023-11-04 15:52:54 -0700

    added ProjectPreview html

---
+++ bootcamp-milestone-2/src/app/components/projectPreview.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import style from "./projectPreview.module.css";
+import Image from "next/image";
+import type { Project } from "../static/projectData";
+import Link from "next/link";
+
+export default function ProjectPreview(props: Project) {
+  return (
+    <div className={style.project}>
+        <Link href={props.slug}>
+                <Image src={props.image} width={617} height={500} alt="project image"/>
+        </Link>
+        <div className={style.project_details}>
+            <strong className={style.project_name}>{props.title}</strong>
+            <p className={style.project_description}>{props.description}</p>
+            <Link href={props.slug}>Learn More</Link>
+        </div>
+    </div>
+  );
+}
