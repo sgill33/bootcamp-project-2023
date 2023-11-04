782ed20b6b06084fe95b3b12b8eb6341ad636870
sgill33 <sgill33@calpoly.edu>
2023-11-04 15:52:29 -0700

    added footer html

---
+++ bootcamp-milestone-2/src/app/components/footer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./footer.module.css";
+
+export default function Footer() {
+  return (
+    // replace everything in between the <header> & <header /> tags
+    // with your navbar code from your earlier milestones
+    <footer className={style.footer}>&copy; 2023 Shawn's Personal Website | All Rights Reserved</footer>
+  );
+}
