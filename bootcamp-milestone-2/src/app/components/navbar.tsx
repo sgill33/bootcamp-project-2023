69ff4d63ae34f4bbe0fa9d39cf75a573d308b7ef
sgill33 <sgill33@calpoly.edu>
2023-11-04 15:52:37 -0700

    added navbar html

--- bootcamp-milestone-2/src/app/components/navbar.tsx
+++ bootcamp-milestone-2/src/app/components/navbar.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import style from "./navbar.module.css";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     // replace everything in between the <header> & <header /> tags
     // with your navbar code from your earlier milestones
     <header className={style.nav} >
-      <nav className="navbar">
-            <h1 className={style.logo}> <a href="index.html">Shawn's Website</a></h1>
+      <nav className={style.navbar}>
+            <h1 className={style.logo}> <a href="./">Shawn's Website</a></h1>
             <ul className={style.navlist}>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="portfolio.html">Portfolio</a></li>
-                <li><a href="resume.html">Resume</a></li>
-                <li><a href="contact.html">Contact</a></li>
+                <li><Link href="./">Home</Link></li>
+                <li><Link href="/blog">Blog</Link></li>
+                <li><Link href="/portfolio">Portfolio</Link></li>
+                <li><Link href="/resume">Resume</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
             </ul>
         </nav>
     </header>
