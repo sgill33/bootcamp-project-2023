6009300bf8321cfa2cdb4b45517ca05675983d25
sgill33 <sgill33@calpoly.edu>
2023-11-20 21:51:29 -0800

    loaded project previews from db

--- bootcamp-milestone-2/src/app/components/projectPreview.tsx
+++ bootcamp-milestone-2/src/app/components/projectPreview.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import style from "./projectPreview.module.css";
 import Image from "next/image";
-import type { Project } from "../static/projectData";
+import type { Project } from "../helper/projectData";
 import Link from "next/link";
+import { IPort } from "../database/portfolioSchema";
 
-export default function ProjectPreview(props: Project) {
+export default function ProjectPreview(props: IPort) {
   return (
     <div className={style.project}>
-        <Link href={props.slug}>
-                <Image src={props.image} width={617} height={500} alt="project image"/>
-        </Link>
-        <div className={style.project_details}>
-            <strong className={style.project_name}>{props.title}</strong>
-            <p className={style.project_description}>{props.description}</p>
-            <Link href={props.slug}>Learn More</Link>
-        </div>
+      <Link href={props.slug}>
+        <Image src={props.image} width={617} height={500} alt="project image" />
+      </Link>
+      <div className={style.project_details}>
+        <strong className={style.project_name}>{props.title}</strong>
+        <p className={style.project_description}>{props.description}</p>
+        <Link href={props.slug}>Learn More</Link>
+      </div>
     </div>
   );
 }
