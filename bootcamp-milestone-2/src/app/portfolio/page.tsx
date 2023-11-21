f4277a70757f4de00e34adb15d50d990a3d0cdba
sgill33 <sgill33@calpoly.edu>
2023-11-20 21:53:41 -0800

    updated portfolio page with database values

--- bootcamp-milestone-2/src/app/portfolio/page.tsx
+++ bootcamp-milestone-2/src/app/portfolio/page.tsx
@@ -1,16 +1,43 @@
-import React from 'react'
-import projects from '../static/projectData'
-import ProjectPreview from '../components/projectPreview'
+import React from "react";
+import projects from "../helper/projectData";
+import ProjectPreview from "../components/projectPreview";
+import connectDB from "../helper/db";
+import portfolioSchema from "../database/portfolioSchema";
 
-export default function Portfolio() {
-  return (
-    <main>
+async function getProjects() {
+  await connectDB();
+
+  try {
+    // query for all blogs and sort by date
+    const projs = await portfolioSchema.find().orFail();
+    // send a response as the blogs as the message
+    return projs;
+  } catch (err) {
+    return null;
+  }
+}
+
+export default async function Portfolio() {
+  const projects = await getProjects();
+  if (projects) {
+    return (
+      <main>
         <h1 className="page-title">Shawn's Portfolio</h1>
-        {projects.map(
-          (project) => (
-            <ProjectPreview {...project} />
-          )
-        )}  
-    </main>
-  )
+        {projects.map((project) => (
+          <ProjectPreview
+            title={project.title}
+            description={project.description}
+            image={project.image}
+            slug={project.slug}
+          />
+        ))}
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <h1>No Projects Found</h1>
+      </main>
+    );
+  }
 }
