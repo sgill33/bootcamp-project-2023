11993fb1ce15ae5d6265e2d7544f2cc2725d64a7
sgill33 <sgill33@calpoly.edu>
2023-11-20 21:51:09 -0800

    fixed homepage link

--- bootcamp-milestone-2/src/app/components/navbar.tsx
+++ bootcamp-milestone-2/src/app/components/navbar.tsx
@@ -8,9 +8,9 @@ export default function Navbar() {
     // with your navbar code from your earlier milestones
     <header className={style.nav} >
       <nav className={style.navbar}>
-            <h1 className={style.logo}> <a href="./">Shawn's Website</a></h1>
+            <h1 className={style.logo}> <a href="/">Shawn's Website</a></h1>
             <ul className={style.navlist}>
-                <li><Link href="./">Home</Link></li>
+                <li><Link href="/">Home</Link></li>
                 <li><Link href="/blog">Blog</Link></li>
                 <li><Link href="/portfolio">Portfolio</Link></li>
                 <li><Link href="/resume">Resume</Link></li>
