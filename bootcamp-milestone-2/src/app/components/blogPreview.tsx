5e49a5cf7c0ffaec98cb01679fd63a52d61a89ab
sgill33 <sgill33@calpoly.edu>
2023-11-20 21:46:06 -0800

    changed link for blog route

--- bootcamp-milestone-2/src/app/components/blogPreview.tsx
+++ bootcamp-milestone-2/src/app/components/blogPreview.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import style from "./blogPreview.module.css";
 import Image from "next/image";
-import type { Blog } from "../static/blogData";
 import Link from "next/link";
+import { IBlog } from "../database/blogSchema";
 
-export default function BlogPreview(props: Blog) {
+
+
+
+export default function BlogPreview(props: IBlog) {
+  
   return (
-
     <div className={style.blogpost}>
       <div>
         <h3 className="blog-title"> {props.title} </h3>
         <Image src={props.image} alt="preview image" width={500} height={350}></Image>
         <p className="blog-text">{props.description}</p>
-        <p className="preview_date">{props.date}</p>
-        <Link href={props.slug}>Read More</Link>
+        <p className="preview_date">{props.date && props.date.toDateString()}</p>
+        <Link href= {`blog/${props.slug}`}>Read More</Link>
       </div>
     </div>
   );
 }
+
