7a9278d48c0b53f31411e17b0a1f000df2ffb3f0
sgill33 <sgill33@calpoly.edu>
2023-11-20 21:44:14 -0800

    added route for blogs

---
+++ bootcamp-milestone-2/src/app/api/blog/[slug]/route.ts
@@ -0,0 +1,21 @@
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/app/helper/db";
+import blogSchema from "../../../database/blogSchema";
+
+type IParams = {
+  params: {
+    slug: string;
+  };
+};
+
+export async function GET(req: NextRequest, { params }: IParams) {
+  await connectDB(); // function from db.ts before
+  const { slug } = params; // another destructure
+
+  try {
+    const blog = await blogSchema.findOne({ slug }).orFail();
+    return NextResponse.json(blog);
+  } catch (err) {
+    return NextResponse.json("Blog not found.", { status: 404 });
+  }
+}
