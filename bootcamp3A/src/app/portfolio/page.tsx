32c1d2fb36e318edf709425da379870930c5afc3
sgill33 <sgill33@calpoly.edu>
2023-12-21 19:27:30 -0800

    added comment submission handling to projects

--- bootcamp3A/src/app/portfolio/page.tsx
+++ bootcamp3A/src/app/portfolio/page.tsx
@@ -1,38 +1,130 @@
-import React from "react";
+'use client';
+import React, { useState } from "react";
 import ProjectPreview from "../components/projectPreview";
-import connectDB from "../helper/db";
-import portfolioSchema from "../database/portfolioSchema";
+import  {IPort} from "../database/portfolioSchema";
+import Comment from "@/app/components/comment";
+import { IComment } from "@/app/database/blogSchema";
 
-async function getProjects() {
-  await connectDB();
 
-  try {
-    // query for all blogs and sort by date
-    const projs = await portfolioSchema.find().orFail();
-    // send a response as the blogs as the message
-    return projs;
-  } catch (err) {
-    return null;
-  }
-}
+
+
 
 export default async function Portfolio() {
+  async function getProjects() {
+    const res = await fetch(`http://localhost:3000/api/portfolio`,
+    {
+      cache:"no-store"
+    });
+  
+    if(res.ok){
+      return res.json()
+    }
+    return null
+  }
+
+  async function getComments() {
+    const res = await fetch(`http://localhost:3000/api/portfolio/comment`,
+      {
+        method:'GET',
+        cache:"no-store"
+      });
+  
+      if(res.ok){
+        return res.json()
+      }
+      return null
+  }
+
+  const [newComment, setComment] = useState<IComment | null>(null);
   const projects = await getProjects();
+  const comments = await getComments();
+
+  console.log(projects)
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    /*
+    Handles form submission by clearing form and appending comment
+    */
+    e.preventDefault();
+
+    try {
+      //get form submission event
+      const formElement = e.target as HTMLFormElement;
+
+        // Access values directly from the form elements
+        const nameInput =
+          formElement.querySelector<HTMLInputElement>(
+            'input[name="name"]'
+          );
+        const descriptionText =
+          formElement.querySelector<HTMLTextAreaElement>(
+            'textarea[name="description"]'
+          );
+
+        // Explicitly cast e.target to HTMLFormElement
+        const newComment: IComment = {
+          user: nameInput?.value || "",
+          comment: descriptionText?.value || "",
+          time: new Date(),
+        };
+
+        //Add comment to db and update UI
+        const response = await fetch(
+          `api/portfolio/comment`, {
+            method: "POST",
+            headers: { 'Content-Type': "application/json" },
+            body: JSON.stringify(newComment),
+          });
+
+        
+        //clear form data
+        if (nameInput) nameInput.value = "";
+        if (descriptionText) descriptionText.value = "";
+
+        if (response.status === 200)
+          setComment({
+            user: newComment?.user || "",
+            comment: newComment?.comment || "",
+            time: newComment?.time || new Date(),
+            
+          });
+        window.location.reload();
+          
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  
   if (projects) {
     return (
       <main>
         <h1 className="page-title">Shawn's Portfolio</h1>
-        {projects.map((project) => (
-          <ProjectPreview
-            title={project.title}
-            description={project.description}
-            image={project.image}
-            slug={project.slug}
-          />
-        ))}
+        {projects?.map((proj : IPort, index: number) => (
+                        <ProjectPreview {...proj}/>
+                    ))}
+        <div className="comments">
+            <h2>Comments</h2>
+        {comments?.map((comment : IComment, index: number) => (
+                        <Comment {...comment}/>
+                    ))}
+        </div>
+        
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" placeholder="Name" required/>
+                <textarea
+                id="description"
+                placeholder="Comment"
+                name="description"
+                ></textarea>
+                <input type="submit" />
+            </form>
+        </div>
       </main>
     );
-  } else {
+  } 
+  else {
     return (
       <main>
         <h1>No Projects Found</h1>
